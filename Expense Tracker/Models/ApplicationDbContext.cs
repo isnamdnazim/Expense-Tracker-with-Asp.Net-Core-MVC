2289b29e44f2e5ccd226afbde34fddae3b7486ac
Md Nazim Uddin <isnamdnazim@gmail.com>
2022-08-07 18:49:17 +0600

    Models Created

---
+++ Expense Tracker/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
